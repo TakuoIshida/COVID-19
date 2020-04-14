a143dad8ab68944b1b78b2276ec426aa4e1ef64c
TakuoIshida <archersparadox.100@gmail.com>
2020-04-14 15:07:17 +0900

    ミスを修正

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -6,23 +6,23 @@ import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
 const CountryPicker = ({ handleCountryChange }) => {
-    const [fetchedCountries, setFetchedCountries] = useState([]);
+    const [Countries, setCountries] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
-            setFetchedCountries(await fetchCountries());
+            setCountries(await fetchCountries());
         }
         fetchAPI();
-    },[setFetchedCountries]);
+    },[]);
 
     return ( 
         <FormControl className={styles.formControl}>
             <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="global"> global </option>
-                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+                <option value=""> Global </option>
+                {Countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
-    )
-}
+    );
+};
 
 export default CountryPicker;
